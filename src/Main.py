52c08af8836144a4002d50dc65c18e9d43ca5945
Matt Umut Ucak <umutucak79@gmail.com>
2022-02-28 07:41:49 +0100

    cleanup + structure + visited flagging fix

--- src/Main.py
+++ src/Main.py
@@ -1,43 +1,42 @@
 from Maze import Maze
 import pygame
-import time
 
-# setup window
-FPS = 30
-S_HEIGHT = 600
-M_HEIGHT = 25
-CELL_SIZE = S_HEIGHT // M_HEIGHT
+# # setup window
+# FPS = 30
+# S_HEIGHT = 600
+M_HEIGHT = 5
+# CELL_SIZE = S_HEIGHT // M_HEIGHT
 
 # maze config
-# maze = Maze(M_HEIGHT, M_HEIGHT)
-# maze.initialize()
+maze = Maze(M_HEIGHT, M_HEIGHT)
+maze.initialize()
 
-# colors
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 225)
-YELLOW = (255, 255, 0)
+# # colors
+# WHITE = (255, 255, 255)
+# GREEN = (0, 255, 0)
+# BLUE = (0, 0, 225)
+# YELLOW = (255, 255, 0)
 
-# pygame init
-pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((S_HEIGHT, S_HEIGHT))
-pygame.display.set_caption("Python Maze")
-clock = pygame.time.Clock()
+# # pygame init
+# pygame.init()
+# pygame.mixer.init()
+# screen = pygame.display.set_mode((S_HEIGHT, S_HEIGHT))
+# pygame.display.set_caption("Python Maze")
+# clock = pygame.time.Clock()
 
-def render_grid():
-	for i in range(0, S_HEIGHT, CELL_SIZE):
-		for j in range(0, S_HEIGHT, CELL_SIZE):
-			rect = pygame.Rect(i, j, CELL_SIZE, CELL_SIZE)
-			pygame.draw.rect(screen, WHITE, rect, 1)
-			pygame.display.update()
+# def render_grid():
+# 	for i in range(0, S_HEIGHT, CELL_SIZE):
+# 		for j in range(0, S_HEIGHT, CELL_SIZE):
+# 			rect = pygame.Rect(i, j, CELL_SIZE, CELL_SIZE)
+# 			pygame.draw.rect(screen, WHITE, rect, 1)
+# 			pygame.display.update()
 
-# render maze
-render_grid()
+# # render maze
+# render_grid()
 
-# game loop
-while True:
-	clock.tick(FPS)
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			break
+# # game loop
+# while True:
+# 	clock.tick(FPS)
+# 	for event in pygame.event.get():
+# 		if event.type == pygame.QUIT:
+# 			break
--- src/Maze.py
+++ src/Maze.py
@@ -7,68 +7,72 @@ class Maze:
 	def __init__(self, height, width):
 		self.height = height
 		self.width = width
+		self.maze = []
 	
 	def initialize(self):
-		maze = []
 		initial = Cell.UPWALL | Cell.RIGHTWALL | Cell.DOWNWALL | Cell.LEFTWALL
 		for i in range(self.height-1): # y
-			maze.append([])
+			self.maze.append([])
 			for j in range(self.width-1): # x
-				maze[i].append(initial)
+				self.maze[i].append(initial)
 
-		return self.DFS(maze)
+		return self.DFS()
 
-	def DFS(self, maze):
+	def DFS(self):
 		stack = []
 		x = 0
 		y = 0
-		stack.append([maze[x][y], x, y]) # not my proudest design
-
+		stack.append([x, y])
 		while(len(stack) > 0):
 			current_cell = stack.pop()
+			self.maze[current_cell[0]][current_cell[1]] |= Cell.VISITED
 			sleep(1)
-			print(current_cell[1],current_cell[2])
-			current_cell[0] |= Cell.VISITED
-			children = self.generate_random_child(current_cell, maze)
+			children = self.generate_random_child(current_cell)
 			if len(children) > 0:
 				stack.append(current_cell) # so that the other children can be explored when we eventually backtrack
 				random_child = children[randint(0, len(children)-1)]
-				unit_v = numpy.subtract((random_child[1], random_child[2]), (current_cell[1], current_cell[2])) # indicates the direction from current->child
-
-				if unit_v[0] == 1: # right
-					current_cell[0] &= Cell.RIGHTWALL
-					random_child[0] &= Cell.LEFTWALL
-				if unit_v[0] == -1: #left
-					current_cell[0] &= Cell.LEFTWALL
-					random_child[0] &= Cell.RIGHTWALL
-				if unit_v[1] == 1: #up
-					current_cell[0] &= Cell.UPWALL
-					random_child[0] &= Cell.DOWNWALL
-				if unit_v[1] == -1: #down
-					current_cell[0] &= Cell.DOWNWALL
-					random_child[0] &= Cell.UPWALL
-
+				unit_v = numpy.subtract((random_child[0], random_child[1]), (current_cell[0], current_cell[1])) # indicates the direction from current->child
+				self.remove_common_wall(current_cell, random_child, unit_v)
 				stack.append(random_child)
 
-		return maze
+		return self.maze
 
-	def generate_random_child(self, parent_cell, maze):
+	def generate_random_child(self, parent_cell):
 		children = []
-		x = parent_cell[1]
-		y = parent_cell[2]
+		x = parent_cell[0]
+		y = parent_cell[1]
+		print('current cell:', x, y)
 
 		if x > 0: #left border
-			if maze[x-1][y] & Cell.VISITED == 0:
-				children.append([maze[x-1][y], x-1, y])
+			if self.maze[x-1][y] & Cell.VISITED == 0:
+				children.append([x-1, y])
 		if y > 0: #up border
-			if maze[x][y-1] & Cell.VISITED == 0:
-				children.append([maze[x][y-1], x, y-1])
+			if self.maze[x][y-1] & Cell.VISITED == 0:
+				children.append([x, y-1])
 		if x < self.width-2: #right border
-			if maze[x+1][y] & Cell.VISITED == 0:
-				children.append([maze[x+1][y], x+1, y])
+			if self.maze[x+1][y] & Cell.VISITED == 0:
+				children.append([x+1, y])
 		if y < self.height-2: #down border
-			print('in if')
-			if maze[x][y+1] & Cell.VISITED == 0:
-				children.append([maze[x][y+1], x, y+1])
+			if self.maze[x][y+1] & Cell.VISITED == 0:
+				children.append([x, y+1])
 
 		return children
+
+	def remove_common_wall(self, current_cell, random_child, unit_v):
+		parent_x = current_cell[0]
+		parent_y = current_cell[1]
+		child_x = random_child[0]
+		child_y = random_child[1]
+
+		if unit_v[0] == 1: # right
+			self.maze[parent_x][parent_y] &= ~Cell.RIGHTWALL
+			self.maze[child_x][child_y] &= ~Cell.LEFTWALL
+		if unit_v[0] == -1: #left
+			self.maze[parent_x][parent_y] &= ~Cell.LEFTWALL
+			self.maze[child_x][child_y] &= ~Cell.RIGHTWALL
+		if unit_v[1] == 1: #up
+			self.maze[parent_x][parent_y] &= ~Cell.UPWALL
+			self.maze[child_x][child_y] &= ~Cell.DOWNWALL
+		if unit_v[1] == -1: #down
+			self.maze[parent_x][parent_y] &= ~Cell.DOWNWALL
+			self.maze[child_x][child_y] &= ~Cell.UPWALL
