e7b5f4b5f7f47fd375c6319cf7fc04435956aaee
Matt Umut Ucak <umutucak79@gmail.com>
2022-03-02 18:34:08 +0100

    maze rendering, very bare bones

--- src/Main.py
+++ src/Main.py
@@ -1,42 +1,77 @@
+from Agent import Agent
 from Maze import Maze
+from Cell import Cell
 import pygame
-
-# # setup window
-# FPS = 30
-# S_HEIGHT = 600
-M_HEIGHT = 5
-# CELL_SIZE = S_HEIGHT // M_HEIGHT
+import sys
 
 # maze config
+M_HEIGHT = 5
 maze = Maze(M_HEIGHT, M_HEIGHT)
 maze.initialize()
 
-# # colors
-# WHITE = (255, 255, 255)
-# GREEN = (0, 255, 0)
-# BLUE = (0, 0, 225)
-# YELLOW = (255, 255, 0)
+agent = Agent((0,0), maze)
+path = agent.BFS((3,3))
+print(path)
+# setup window
+FPS = 30
+S_HEIGHT = 600
+CELL_SIZE = S_HEIGHT // M_HEIGHT
 
-# # pygame init
-# pygame.init()
-# pygame.mixer.init()
-# screen = pygame.display.set_mode((S_HEIGHT, S_HEIGHT))
-# pygame.display.set_caption("Python Maze")
-# clock = pygame.time.Clock()
+# colors
+WHITE = (255, 255, 255)
+BLACK  = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 225)
 
-# def render_grid():
-# 	for i in range(0, S_HEIGHT, CELL_SIZE):
-# 		for j in range(0, S_HEIGHT, CELL_SIZE):
-# 			rect = pygame.Rect(i, j, CELL_SIZE, CELL_SIZE)
-# 			pygame.draw.rect(screen, WHITE, rect, 1)
-# 			pygame.display.update()
+# pygame init
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((S_HEIGHT, S_HEIGHT))
+screen.fill(WHITE)
+pygame.display.set_caption("Python Maze")
+clock = pygame.time.Clock()
 
-# # render maze
-# render_grid()
+# render the maze
+def render_maze():
+	# x = 0
+	# y = 0
+	# for i in range(M_HEIGHT):
+	# 	for j in range(M_HEIGHT):
+	# 		cell = maze.get_value(i, j)
+	# 		if cell & Cell.UPWALL == 1:
+	# 			pygame.draw.line(screen, BLACK, [x, y], [x + CELL_SIZE, y], 5)
+	# 		if cell & Cell.LEFTWALL == 8:
+	# 			pygame.draw.line(screen, BLACK, [x, y], [x, y + CELL_SIZE])
+	# 		if i == M_HEIGHT-1:
+	# 			if cell & Cell.RIGHTWALL == 2:
+	# 				pygame.draw.line(screen, BLACK, [x + CELL_SIZE, y], [x + CELL_SIZE, y + CELL_SIZE])
+	# 		if j == M_HEIGHT-1:
+	# 			if cell & Cell.DOWNWALL ==  4:
+	# 				pygame.draw.line(screen, BLACK, [x, y], [x, y + CELL_SIZE])
+	# 		y += CELL_SIZE
+	# 	x += CELL_SIZE
 
-# # game loop
-# while True:
-# 	clock.tick(FPS)
-# 	for event in pygame.event.get():
-# 		if event.type == pygame.QUIT:
-# 			break
+	x = 0
+	y = 0
+	for i in range(M_HEIGHT):
+		for j in range(M_HEIGHT):
+			cell = maze.get_value(i, j)
+			print(cell)
+			x += CELL_SIZE
+		y += CELL_SIZE
+	
+	pygame.display.update()
+
+
+def traverse_maze():
+	pass
+
+render_maze()
+
+# game loop
+while True:
+	clock.tick(FPS)
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit(0)
