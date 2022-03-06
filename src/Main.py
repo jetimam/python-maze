9c3b92fd16abfbe84ab5e74add8c45b013967168
Matt Umut Ucak <umutucak@protonmail.com>
2022-03-06 10:21:57 +0100

    maze rendering improved

--- src/Main.py
+++ src/Main.py
@@ -5,12 +5,12 @@ import pygame
 import sys
 
 # maze config
-M_HEIGHT = 5
+M_HEIGHT = 20
 maze = Maze(M_HEIGHT, M_HEIGHT)
 maze.initialize()
 
 agent = Agent((0,0), maze)
-path = agent.BFS((3,3))
+path = agent.BFS((15,15))
 print(path)
 # setup window
 FPS = 30
@@ -34,32 +34,24 @@ clock = pygame.time.Clock()
 
 # render the maze
 def render_maze():
-	# x = 0
-	# y = 0
-	# for i in range(M_HEIGHT):
-	# 	for j in range(M_HEIGHT):
-	# 		cell = maze.get_value(i, j)
-	# 		if cell & Cell.UPWALL == 1:
-	# 			pygame.draw.line(screen, BLACK, [x, y], [x + CELL_SIZE, y], 5)
-	# 		if cell & Cell.LEFTWALL == 8:
-	# 			pygame.draw.line(screen, BLACK, [x, y], [x, y + CELL_SIZE])
-	# 		if i == M_HEIGHT-1:
-	# 			if cell & Cell.RIGHTWALL == 2:
-	# 				pygame.draw.line(screen, BLACK, [x + CELL_SIZE, y], [x + CELL_SIZE, y + CELL_SIZE])
-	# 		if j == M_HEIGHT-1:
-	# 			if cell & Cell.DOWNWALL ==  4:
-	# 				pygame.draw.line(screen, BLACK, [x, y], [x, y + CELL_SIZE])
-	# 		y += CELL_SIZE
-	# 	x += CELL_SIZE
-
 	x = 0
 	y = 0
 	for i in range(M_HEIGHT):
 		for j in range(M_HEIGHT):
 			cell = maze.get_value(i, j)
-			print(cell)
-			x += CELL_SIZE
-		y += CELL_SIZE
+			if cell & Cell.UPWALL == 1:
+				pygame.draw.line(screen, BLACK, [x, y], [x + CELL_SIZE, y])
+			if cell & Cell.LEFTWALL == 8:
+				pygame.draw.line(screen, BLACK, [x, y], [x, y + CELL_SIZE])
+			if i == M_HEIGHT-1:
+				if cell & Cell.RIGHTWALL == 2:
+					pygame.draw.line(screen, BLACK, [x + CELL_SIZE, y], [x + CELL_SIZE, y + CELL_SIZE])
+			if j == M_HEIGHT-1:
+				if cell & Cell.DOWNWALL ==  4:
+					pygame.draw.line(screen, BLACK, [x, y], [x, y + CELL_SIZE])
+			y += CELL_SIZE
+		y = 0
+		x += CELL_SIZE
 	
 	pygame.display.update()
 
