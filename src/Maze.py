1c546643957acf0c0560f608e6ec93d09921556b
Matt Umut Ucak <umutucak@protonmail.com>
2022-03-07 16:23:39 +0100

    cleanup + fixed BFS children generation

--- src/Maze.py
+++ src/Maze.py
@@ -3,15 +3,13 @@ from random import randint
 from Position import Position
 
 class Maze:
-	def __init__(self, height, width):
+	def __init__(self, height):
 		self.height = height
-		self.width = width
 		self.maze = []
 	
 	def initialize(self):
 		initial = Cell.UPWALL | Cell.RIGHTWALL | Cell.DOWNWALL | Cell.LEFTWALL # 0 1111
 		self.maze = [[initial for _ in range(self.height)] for _ in range(self.height)]
-
 		return self.DFS()
 
 	def DFS(self):
@@ -28,48 +26,31 @@ class Maze:
 				random_child_position = children[randint(0, len(children)-1)]
 				unit_v = random_child_position.subtract(current_cell_position) # indicates the direction from current->child
 				self.remove_common_wall(current_cell_position, random_child_position, unit_v)
-				print(current_cell_position, self.maze[current_cell_position.x][current_cell_position.y])
+				# print(current_cell_position, self.maze[current_cell_position.x][current_cell_position.y])
 				stack.append(random_child_position)
 
 		return self.maze
 
 	def generate_children_maze_build(self, current_cell_position):
 		children = []
-
-		if current_cell_position.x > 0: #left border
-			if self.maze[current_cell_position.x - 1][current_cell_position.y] & Cell.VISITED == 0:
-				children.append(Position(current_cell_position.x - 1, current_cell_position.y))
-		if current_cell_position.y > 0: #up border
-			if self.maze[current_cell_position.x][current_cell_position.y - 1] & Cell.VISITED == 0:
-				children.append(Position(current_cell_position.x, current_cell_position.y - 1))
-		if current_cell_position.x <= self.width - 2: #right border
-			if self.maze[current_cell_position.x + 1][current_cell_position.y] & Cell.VISITED == 0:
-				children.append(Position(current_cell_position.x + 1, current_cell_position.y))
-		if current_cell_position.y <= self.height - 2: #down border
-			if self.maze[current_cell_position.x][current_cell_position.y + 1] & Cell.VISITED == 0:
-				children.append(Position(current_cell_position.x, current_cell_position.y + 1))
-
-		return children
-
-	def generate_children_agent_traversal(self, current_cell_position, visited):
-		children = []
 		x = current_cell_position.x
 		y = current_cell_position.y
 
 		if x > 0: #left border
-			if Position(x-1,y) not in visited and self.maze[x-1][y] & Cell.LEFTWALL == 0:
-				children.append(Position(x-1, y))
+			if self.maze[x - 1][y] & Cell.VISITED == 0:
+				children.append(Position(x - 1, y))
 		if y > 0: #up border
-			if Position(x,y-1) not in visited and self.maze[x][y-1] & Cell.UPWALL == 0:
-				children.append(Position(x, y-1))
-		if x < self.width-2: #right border
-			if Position(x+1,y) not in visited and self.maze[x+1][y] & Cell.RIGHTWALL == 0:
-				children.append(Position(x+1, y))
-		if y < self.height-2: #down border
-			if Position(x,y+1) not in visited and self.maze[x][y+1] & Cell.DOWNWALL == 0:
-				children.append(Position(x, y+1))
+			if self.maze[x][y - 1] & Cell.VISITED == 0:
+				children.append(Position(x, y - 1))
+		if x <= self.height - 2: #right border
+			if self.maze[x + 1][y] & Cell.VISITED == 0:
+				children.append(Position(x + 1, y))
+		if y <= self.height - 2: #down border
+			if self.maze[x][y + 1] & Cell.VISITED == 0:
+				children.append(Position(x, y + 1))
 
 		return children
+
 
 	def remove_common_wall(self, current_cell_position, random_child_position, unit_v):
 		if unit_v.x == 1: # right
@@ -84,6 +65,26 @@ class Maze:
 		elif unit_v.y == -1: #up
 			self.maze[current_cell_position.x][current_cell_position.y] &= ~Cell.UPWALL
 			self.maze[random_child_position.x][random_child_position.y] &= ~Cell.DOWNWALL
+
+	def generate_children_agent_traversal(self, current_cell_position, visited):
+		children = []
+		x = current_cell_position.x
+		y = current_cell_position.y
+
+		if x > 0: #left border
+			if Position(x-1,y) not in visited and self.maze[x-1][y] & Cell.RIGHTWALL == 0:
+				children.append(Position(x-1, y))
+		if y > 0: #up border
+			if Position(x,y-1) not in visited and self.maze[x][y-1] & Cell.DOWNWALL == 0:
+				children.append(Position(x, y-1))
+		if x < self.height-2: #right border
+			if Position(x+1,y) not in visited and self.maze[x+1][y] & Cell.LEFTWALL == 0:
+				children.append(Position(x+1, y))
+		if y < self.height-2: #down border
+			if Position(x,y+1) not in visited and self.maze[x][y+1] & Cell.UPWALL == 0:
+				children.append(Position(x, y+1))
+
+		return children
 
 	def get_value(self, i, j):
 		return self.maze[i][j]
