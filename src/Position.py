3c3261300275cc34a96494c0fc915d324a83fe00
Matt Umut Ucak <umutucak@protonmail.com>
2022-03-17 13:27:05 +0100

    optimized priorityqueue

--- src/Position.py
+++ src/Position.py
@@ -12,5 +12,10 @@ class Position:
 	def __eq__(self, __o: object) -> bool:
 		return __o and self.x == __o.x and self.y == __o.y
 
+	def __lt__ (self, other):
+		if self.x == other.x:
+			return self.y < other.y
+		return self.x < other.x
+
 	def subtract(self, parent):
 		return Position(self.x - parent.x, self.y - parent.y)
