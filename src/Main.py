eca746df24cfae8403008332844cfe92725d2ccd
Matt Umut Ucak <umutucak@protonmail.com>
2022-03-31 13:36:50 +0200

    merge conflict fix

--- src/Main.py
+++ src/Main.py
@@ -2,71 +2,59 @@ from Agent import Agent
 from Maze import Maze
 from Cell import Cell
 from Position import Position
+import Util
 import pygame
 import sys
 import math
 
 # maze config
-M_HEIGHT = 25
-maze = Maze(M_HEIGHT)
+maze = Maze(Util.M_HEIGHT)
 maze.initialize()
 
-# setup window
-FPS = 5
-S_HEIGHT = 800
-CELL_SIZE = S_HEIGHT // M_HEIGHT
-
 # agent config
-agent = Agent(Position(M_HEIGHT-1, M_HEIGHT-1), maze)
-A_SIZE = CELL_SIZE // 4
+agent = Agent(Position(Util.M_HEIGHT-1, Util.M_HEIGHT-1), maze)
+A_SIZE = Util.CELL_SIZE // 4
 
 # manual user config
 user_pos = Position(0, 0)
 
-# colors
-WHITE = (255, 255, 255)
-BLACK  = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 225)
-
 # pygame init
 pygame.init()
 pygame.mixer.init()
-screen = pygame.display.set_mode((S_HEIGHT, S_HEIGHT))
-screen.fill(WHITE)
+screen = pygame.display.set_mode((Util.S_HEIGHT, Util.S_HEIGHT))
+screen.fill(Util.WHITE)
 pygame.display.set_caption("Python Maze")
 clock = pygame.time.Clock()
-
-def translate_coordinates(maze_position):
-	return (maze_position+0.5) * (CELL_SIZE)
 
 def render_maze():
 	x = 0
 	y = 0
-	for i in range(M_HEIGHT):
-		for j in range(M_HEIGHT):
+	for i in range(Util.M_HEIGHT):
+		for j in range(Util.M_HEIGHT):
 			cell = maze.get_value(i, j)
 			if cell & Cell.UPWALL == 1:
-				pygame.draw.line(screen, BLACK, [x, y], [x + CELL_SIZE, y])
+				pygame.draw.line(screen, Util.BLACK, [x, y], [x + Util.CELL_SIZE, y])
 			if cell & Cell.RIGHTWALL == 2:
-				pygame.draw.line(screen, BLACK, [x + CELL_SIZE, y], [x + CELL_SIZE, y + CELL_SIZE])
+				pygame.draw.line(screen, Util.BLACK, [x + Util.CELL_SIZE, y], [x + Util.CELL_SIZE, y + Util.CELL_SIZE])
 			if cell & Cell.DOWNWALL ==  4:
-				pygame.draw.line(screen, BLACK, [x, y + CELL_SIZE], [x + CELL_SIZE, y + CELL_SIZE])
+				pygame.draw.line(screen, Util.BLACK, [x, y + Util.CELL_SIZE], [x + Util.CELL_SIZE, y + Util.CELL_SIZE])
 			if cell & Cell.LEFTWALL == 8:
-				pygame.draw.line(screen, BLACK, [x, y], [x, y + CELL_SIZE])
-			y += CELL_SIZE
+				pygame.draw.line(screen, Util.BLACK, [x, y], [x, y + Util.CELL_SIZE])
+			y += Util.CELL_SIZE
 		y = 0
-		x += CELL_SIZE
+		x += Util.CELL_SIZE
 	
 	pygame.display.update()
 
+def debug_viz(path):
+	pass
+
 def render_agent():
-	pygame.draw.circle(screen, RED, [translate_coordinates(agent.position.x), translate_coordinates(agent.position.y)], A_SIZE)
+	pygame.draw.circle(screen, Util.RED, [Util.translate_coordinates(agent.position.x), Util.translate_coordinates(agent.position.y)], A_SIZE)
 	pygame.display.update()
 
 def render_user():
-	pygame.draw.circle(screen, BLUE, [translate_coordinates(user_pos.x), translate_coordinates(user_pos.y)], A_SIZE)
+	pygame.draw.circle(screen, Util.BLUE, [Util.translate_coordinates(user_pos.x), Util.translate_coordinates(user_pos.y)], A_SIZE)
 	pygame.display.update()
 
 def can_move(direction):
@@ -84,66 +72,89 @@ def can_move(direction):
 		return cell & Cell.LEFTWALL != 8
 
 def move_up():
-	pygame.draw.circle(screen, WHITE, [translate_coordinates(user_pos.x), translate_coordinates(user_pos.y)], A_SIZE)
+	pygame.draw.circle(screen, Util.WHITE, [Util.translate_coordinates(user_pos.x), Util.translate_coordinates(user_pos.y)], A_SIZE)
 	user_pos.y = user_pos.y - 1
-	pygame.draw.circle(screen, BLUE, [translate_coordinates(user_pos.x), translate_coordinates(user_pos.y)], A_SIZE)
+	pygame.draw.circle(screen, Util.BLUE, [Util.translate_coordinates(user_pos.x), Util.translate_coordinates(user_pos.y)], A_SIZE)
 	pygame.display.update()
 
 def move_left():
-	pygame.draw.circle(screen, WHITE, [translate_coordinates(user_pos.x), translate_coordinates(user_pos.y)], A_SIZE)
+	pygame.draw.circle(screen, Util.WHITE, [Util.translate_coordinates(user_pos.x), Util.translate_coordinates(user_pos.y)], A_SIZE)
 	user_pos.x = user_pos.x - 1
-	pygame.draw.circle(screen, BLUE, [translate_coordinates(user_pos.x), translate_coordinates(user_pos.y)], A_SIZE)
+	pygame.draw.circle(screen, Util.BLUE, [Util.translate_coordinates(user_pos.x), Util.translate_coordinates(user_pos.y)], A_SIZE)
 	pygame.display.update()
 
 def move_down():
-	pygame.draw.circle(screen, WHITE, [translate_coordinates(user_pos.x), translate_coordinates(user_pos.y)], A_SIZE)
+	pygame.draw.circle(screen, Util.WHITE, [Util.translate_coordinates(user_pos.x), Util.translate_coordinates(user_pos.y)], A_SIZE)
 	user_pos.y = user_pos.y + 1
-	pygame.draw.circle(screen, BLUE, [translate_coordinates(user_pos.x), translate_coordinates(user_pos.y)], A_SIZE)
+	pygame.draw.circle(screen, Util.BLUE, [Util.translate_coordinates(user_pos.x), Util.translate_coordinates(user_pos.y)], A_SIZE)
 	pygame.display.update()
 
 def move_right():
-	pygame.draw.circle(screen, WHITE, [translate_coordinates(user_pos.x), translate_coordinates(user_pos.y)], A_SIZE)
+	pygame.draw.circle(screen, Util.WHITE, [Util.translate_coordinates(user_pos.x), Util.translate_coordinates(user_pos.y)], A_SIZE)
 	user_pos.x = user_pos.x + 1
-	pygame.draw.circle(screen, BLUE, [translate_coordinates(user_pos.x), translate_coordinates(user_pos.y)], A_SIZE)
+	pygame.draw.circle(screen, Util.BLUE, [Util.translate_coordinates(user_pos.x), Util.translate_coordinates(user_pos.y)], A_SIZE)
 	pygame.display.update()
 
 def ai_traversal():
 	euclidean = lambda x,y,x_t,y_t: math.sqrt((x_t - x)**2 + (y_t - y)**2)
 	manhattan = lambda x, y, x_t, y_t: abs(x - x_t) + abs(y - y_t)
-	path = agent.search_AS(user_pos, manhattan)
-	pygame.draw.circle(screen, WHITE, [translate_coordinates(agent.position.x), translate_coordinates(agent.position.y)], A_SIZE)
+	# path = agent.search_BFS(user_pos, False, screen)
+	path = agent.search_AS(user_pos, manhattan, False, screen)
+	pygame.draw.circle(screen, Util.WHITE, [Util.translate_coordinates(agent.position.x), Util.translate_coordinates(agent.position.y)], A_SIZE)
 	agent.position = path[1]
-	pygame.draw.circle(screen, RED, [translate_coordinates(agent.position.x), translate_coordinates(agent.position.y)], A_SIZE)
+	pygame.draw.circle(screen, Util.RED, [Util.translate_coordinates(agent.position.x), Util.translate_coordinates(agent.position.y)], A_SIZE)
 	pygame.display.update()
+
+def debug(type):
+	render_maze()
+	while True:
+		clock.tick(Util.FPS)
+		if type == "bfs":
+			agent.search_BFS(user_pos, True, screen)
+			type = 'used'
+		elif type == "as_euclidean":
+			euclidean = lambda x,y,x_t,y_t: math.sqrt((x_t - x)**2 + (y_t - y)**2)
+			agent.search_AS(user_pos, euclidean, True, screen)
+			type = 'used'
+		elif type == "as_manhattan":
+			manhattan = lambda x, y, x_t, y_t: abs(x - x_t) + abs(y - y_t)
+			agent.search_AS(user_pos, manhattan, True, screen)
+			type = 'used'
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				sys.exit()
 
 def game():
 	render_maze()
 	render_agent()
 	render_user()
-	held_key = None
+
 	while True:
-		clock.tick(FPS)
-		if user_pos == agent.position:
-			pygame.quit()
-			print('GG')
-			sys.exit()
+		clock.tick(Util.FPS)
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				pygame.quit()
 				sys.exit()
-			elif event.type == pygame.KEYDOWN:
-				held_key = event.key
+			if user_pos == agent.position:
+				pygame.quit()
+				print('GG')
+				sys.exit()
+			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_ESCAPE:
 					pygame.quit()
 					sys.exit()
-		if held_key == pygame.K_w and can_move('up'):
-			move_up()
-		elif held_key == pygame.K_a and can_move('left'):
-			move_left()
-		elif held_key == pygame.K_s and can_move('down'):
-			move_down()
-		elif held_key == pygame.K_d and can_move('right'):
-			move_right()
-		ai_traversal()
+				if event.key == pygame.K_w and can_move('up'):
+					move_up()
+				elif event.key == pygame.K_a and can_move('left'):
+					move_left()
+				elif event.key == pygame.K_s and can_move('down'):
+					move_down()
+				elif event.key == pygame.K_d and can_move('right'):
+					move_right()
+				else:
+					continue
+				ai_traversal()
 
 game()
+# debug("bfs")
